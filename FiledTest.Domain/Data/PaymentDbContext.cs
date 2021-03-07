bc32b957d6aaca1f376e86ef397ed10ffea4fa31
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-03-08 00:29:50 +0100

    add Migrations and update database

---
+++ FiledTest.Domain/Data/PaymentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FiledTest.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiledTest.Domain.Data
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options) { }
+
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<PaymentState> PaymentStates { get; set; }
+    }
+}
---
+++ FiledTest.Domain/Migrations/20210307232450_initial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FiledTest.Domain.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PaymentStateId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "TEXT", maxLength: 16, nullable: false),
+                    CardHolder = table.Column<string>(type: "TEXT", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SecurityCode = table.Column<string>(type: "TEXT", nullable: true),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_PaymentStates_PaymentStateId",
+                        column: x => x.PaymentStateId,
+                        principalTable: "PaymentStates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_PaymentStateId",
+                table: "Payments",
+                column: "PaymentStateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "PaymentStates");
+        }
+    }
+}
